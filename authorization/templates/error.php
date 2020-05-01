3bd03bf506e3e2e7dd1b1fb94188f5eea3b66f43
Juan Manuel <jmouriz@gmail.com>
2020-05-01 00:22:47 -0300

    Agrego una pantalla de error

---
+++ authorization/templates/error.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+   <head>
+      <base href="../../" />
+      <meta charset="utf-8" />
+      <title>Iniciar sesión</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      <link rel="shortcut icon" href="resources/images/application.ico">
+      <link rel="stylesheet" href="resources/styles/fonts.css" />
+      <link rel="stylesheet" href="resources/styles/default.css" />
+   </head>
+   <body>
+      <div class="container">
+         <form method="post">
+	    <h3 class="error"><?php print($parameters['error']); ?></h3>
+	    <p><?php print($parameters['description']); ?></p>
+         </div>
+      </center>
+   <body>
+</html>
